a990086febd06b55fbf6a612e95d63aa0932dfe4
Alex Milstead <alex@xanderal.com>
2011-04-16 02:03:33 -0400

    MORTHINGS AND STUFF AND JUNK

---
+++ src/com/gitmad/peepshow/exceptions/IncompleteMetadataException.java
@@ -0,0 +1,8 @@
+package com.gitmad.peepshow.exceptions;
+
+/**
+ * TODO: Enter class description.
+ */
+public class IncompleteMetadataException extends InvalidMetadataException {
+    private static final long serialVersionUID = 1L;
+}
